02eddbbbbf6afd1da48c7abed403f4da16c431e9
vabgalimov <vabgalimov@vivaldi.net>
2022-06-22 21:47:14 +0500

    Change yaml file to strings

--- py3rbot/handlers/utils.py
+++ py3rbot/handlers/utils.py
@@ -7,15 +7,15 @@ from ..constants import MAX_MESSAGE_TEXT_LENGTH
 
 def get_formatted(result: Optional[str]) -> str:
     if result is None:
-        return html_italic(strings.was_terminated())
+        return html_italic(strings.was_terminated)
 
     result = result.strip()
 
     if not result:
-        return html_italic(strings.empty())
+        return html_italic(strings.empty)
 
     if len(result) > MAX_MESSAGE_TEXT_LENGTH:
-        return html_italic(strings.too_long_output())
+        return html_italic(strings.too_long_output)
 
     return html.escape(result)
 
---
+++ py3rbot/strings.py
@@ -0,0 +1,43 @@
+start = "Hi! I'm Python Bot. Usage: /help"
+
+help = """\
+Usage:
+  - Reply to me python code
+  - /py command (help: /py)
+  - via me (help: /inline)
+Python version: {python_version}
+"""
+
+py_cmd_help = """\
+Usage:
+  /py [/<args>] <code>
+Example:
+  /py print("Hi!")
+Argmunets:
+  e - Eval mode (help: /eval)
+"""
+
+inline_help = """\
+Usage:
+  @{botname} [/<args>] <code>
+Example:
+  @{botname} print(6 * 7)
+Arguments:
+  e - Eval mode (help: /eval)
+  r - Remove code from result message
+"""
+
+eval_help = """\
+Eval mode as print(eval(...))
+Default imported math module
+Example:
+  /e math.sqrt(4 + 5) # output: 3.0
+"""
+
+empty = "empty"
+too_long_output = "too long output"
+was_terminated = "execution time exceeded"
+
+output = "Output"
+run_code = "Run code"
+running = "Running..."
